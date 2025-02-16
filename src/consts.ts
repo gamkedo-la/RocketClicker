df783e63fb13143631576aad6ea454b203f38eda
Victor Debone <victor@debone.com.br>
2025-02-16 09:59:34 +0100

    feat: adding color palette
    you can import the palette into your preferred software using the .hex file, and use the consts in code

--- src/consts.ts
+++ src/consts.ts
@@ -7,3 +7,34 @@ export const TEXT_STYLE = {
   fontFamily: "DotGothic16",
   fontSize: "32px",
 };
+
+// Color pallete from https://lospec.com/gallery/hby/the-fallen
+export const COLORS = [
+  0x000000, 0x170f17, 0x181931, 0x211424, 0x192d38, 0x45232e, 0x783939,
+  0x3b274d, 0x4b344e, 0x4a3d78, 0x7b6f62, 0x9e4e4e, 0x607b9b, 0x865099,
+  0xd65480, 0x16e1ff, 0xda8a8a, 0xf68080, 0xf4cbd4, 0xffffff,
+];
+
+// Colors names from https://hue.tools
+export const COLORS_NAMES = {
+  black: 0x000000,
+  "dark-void": 0x170f17,
+  "dark-knight": 0x181931,
+  umbra: 0x211424,
+  "elite-teal": 0x192d38,
+  castro: 0x45232e,
+  "cuba-libre": 0x783939,
+  "stormy-night": 0x3b274d,
+  "shadow-purple": 0x4b344e,
+  meteorite: 0x4a3d78,
+  namibia: 0x7b6f62,
+  chutney: 0x9e4e4e,
+  "montreux-blue": 0x607b9b,
+  "vicious-violet": 0x865099,
+  "fever-dream": 0xd65480,
+  "vaporwave-blue": 0x16e1ff,
+  "peaches-of-immortality": 0xda8a8a,
+  "strawberry-field": 0xf68080,
+  "pleasing-pink": 0xf4cbd4,
+  white: 0xffffff,
+};
