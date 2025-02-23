77c3093fb10d461834bbd18274e718861863c5fa
Victor Debone <victor@debone.com.br>
2025-02-23 01:38:09 +0100

    feat: counter component

---
+++ src/scenes/hud/components/counter.tsx
@@ -0,0 +1,70 @@
+import { RESOURCES } from "@game/assets";
+import { STRING_COLORS_NAMES } from "@game/consts";
+import { effect } from "@game/core/signals/signals";
+import { Signal } from "@game/core/signals/types";
+import { Flex } from "@game/core/ui/Flex";
+import { FlexRow } from "@game/core/ui/FlexRow";
+
+export const Counter = ({
+  title,
+  value,
+}: {
+  title: string;
+  value: Signal<number>;
+}) => {
+  const nineSlice = (
+    <nineslice
+      texture={RESOURCES["emboss-button"]}
+      leftWidth={8}
+      rightWidth={8}
+      topHeight={6}
+      bottomHeight={6}
+    />
+  );
+
+  const counterTitle = (
+    <text
+      text={title}
+      resolution={2}
+      style={{
+        fontFamily: "handjet",
+        fontSize: "18px",
+        color: STRING_COLORS_NAMES["cuba-libre"],
+        stroke: STRING_COLORS_NAMES["cuba-libre"],
+        strokeThickness: 1,
+      }}
+    />
+  );
+
+  const counterText: Phaser.GameObjects.Text = (
+    <text
+      text="000000000"
+      resolution={2}
+      style={{
+        fontFamily: "handjet",
+        fontSize: "18px",
+        color: STRING_COLORS_NAMES["vaporwave-blue"],
+      }}
+    />
+  );
+
+  const counterFlex: FlexRow = (
+    <Flex width={64} backgroundElement={nineSlice}>
+      {counterText}
+    </Flex>
+  );
+
+  const flex: FlexRow = (
+    <Flex padding={0}>
+      {counterTitle}
+      {counterFlex}
+    </Flex>
+  );
+
+  effect(() => {
+    counterText.setText(value.get().toString(10).padStart(9, "0"));
+    flex.layout();
+  });
+
+  return flex;
+};
