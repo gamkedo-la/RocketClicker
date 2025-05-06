68a3319d05d8edde8e4f8b3c05e255ed077690dd
Victor Debone <victor@debone.com.br>
2025-05-06 23:43:35 +0200

    feat: resources update in steps, so it's easier to understand what is happening

--- src/systems/MaterialsSystem.ts
+++ src/systems/MaterialsSystem.ts
@@ -17,7 +17,8 @@ export default class MaterialsSystem implements System {
   material_storage: Record<keyof typeof MATERIALS, Signal<number>>;
 
   tickTimer = 0;
-  tickLength = 500;
+  tickLength = 200;
+  currentMaterialIndex = 0;
 
   constructor(private gameState: GameStateManager) {
     gameState.state.subscribe((state) => {
@@ -34,13 +35,15 @@ export default class MaterialsSystem implements System {
     if (this.tickTimer >= this.tickLength) {
       this.tickTimer = 0;
 
-      // TODO: improve how this is displayed
-      this.material_storage[MATERIALS.kWh].set(0);
+      if (this.currentMaterialIndex === 0) {
+        this.material_storage[MATERIALS.kWh].set(0);
+      }
 
       const grid_buildings = this.gameState.state.get()?.board.grid_buildings;
 
       const spin_velocity =
         this.gameState.state.get()?.comet_spin_velocity_abs.get() ?? 0;
+
       // The tax is 0 at 20, and 1 at 40
       const speed_buildings_tax = Math.max(
         Math.min(
@@ -61,84 +64,89 @@ export default class MaterialsSystem implements System {
         0
       );
 
-      MATERIALS_GENERATION_ORDER.forEach((material_order) => {
-        grid_buildings.forEach((buildingSignal) => {
-          const building = buildingSignal.get();
+      const material_to_process =
+        MATERIALS_GENERATION_ORDER[this.currentMaterialIndex];
 
-          if (building === null) return;
+      grid_buildings.forEach((buildingSignal) => {
+        const building = buildingSignal.get();
 
-          // TODO: Fuel cell is evaluating twice because it outputs twice
-          if (
-            building.output[material_order] === undefined ||
-            (material_order === MATERIALS.H2O && building.name === "Fuel Cell")
-          ) {
-            return;
-          }
+        if (building === null) return;
 
-          let successRate =
-            // damp and vibrate are affected by the success rate
-            building.effects.includes(EFFECTS.DAMP) ||
-            building.effects.includes(EFFECTS.VIBRATION)
-              ? building.maximum_success_rate.get()
-              : Math.min(
-                  building.maximum_success_rate.get(),
-                  building.current_efficiency.get()
-                );
+        // TODO: Fuel cell is evaluating twice because it outputs twice
+        if (
+          building.output[material_to_process] === undefined ||
+          (material_to_process === MATERIALS.H2O &&
+            building.name === "Fuel Cell")
+        ) {
+          return;
+        }
 
-          // We need to apply the speed effect here because speed and vibration are combined
-          if (building.effects.includes(EFFECTS.SPEED)) {
-            successRate = Math.min(
-              building.maximum_success_rate.get(),
-              speed_buildings_tax
-            );
-          } else if (building.effects.includes(EFFECTS.HIGH_SPEED)) {
-            successRate = Math.min(
-              building.maximum_success_rate.get(),
-              high_speed_buildings_tax
-            );
-          }
-
-          Object.entries(building.input).forEach(([input, value]) => {
-            // How much of the input material is available?
-            const material =
-              this.material_storage[input as keyof typeof MATERIALS].get();
-            // How much of the input material is needed?
-            successRate = Math.min(successRate, material / value);
-
-            if (input === MATERIALS.kWh) {
-              building.missing_energy = material === 0;
-            } else {
-              building.missing_materials = material === 0;
-            }
-          });
-
-          building.current_success_rate.set(successRate);
-
+        let successRate =
           // damp and vibrate are affected by the success rate
-          if (
-            building.effects.includes(EFFECTS.DAMP) ||
-            building.effects.includes(EFFECTS.VIBRATION)
-          ) {
-            building.current_efficiency.set(successRate);
+          building.effects.includes(EFFECTS.DAMP) ||
+          building.effects.includes(EFFECTS.VIBRATION)
+            ? building.maximum_success_rate.get()
+            : Math.min(
+                building.maximum_success_rate.get(),
+                building.current_efficiency.get()
+              );
+
+        // We need to apply the speed effect here because speed and vibration are combined
+        if (building.effects.includes(EFFECTS.SPEED)) {
+          successRate = Math.min(
+            building.maximum_success_rate.get(),
+            speed_buildings_tax
+          );
+        } else if (building.effects.includes(EFFECTS.HIGH_SPEED)) {
+          successRate = Math.min(
+            building.maximum_success_rate.get(),
+            high_speed_buildings_tax
+          );
+        }
+
+        Object.entries(building.input).forEach(([input, value]) => {
+          // How much of the input material is available?
+          const material =
+            this.material_storage[input as keyof typeof MATERIALS].get();
+          // How much of the input material is needed?
+          successRate = Math.min(successRate, material / value);
+
+          if (input === MATERIALS.kWh) {
+            building.missing_energy = material === 0;
+          } else {
+            building.missing_materials = material === 0;
           }
+        });
 
-          if (!successRate) return;
+        building.current_success_rate.set(successRate);
 
-          Object.entries(building.input).forEach(([input, value]) => {
-            // Subtract the input material from the available material
-            this.material_storage[input as keyof typeof MATERIALS].update(
-              (material) =>
-                Math.max(material - Math.floor(value * successRate), 0)
-            );
-          });
+        // damp and vibrate are affected by the success rate
+        if (
+          building.effects.includes(EFFECTS.DAMP) ||
+          building.effects.includes(EFFECTS.VIBRATION)
+        ) {
+          building.current_efficiency.set(successRate);
+        }
 
-          Object.entries(building.output).forEach(([output, value]) => {
-            this.material_storage[output as keyof typeof MATERIALS].update(
-              (material) => material + Math.floor(value * successRate)
-            );
-          });
+        if (!successRate) return;
+
+        Object.entries(building.input).forEach(([input, value]) => {
+          // Subtract the input material from the available material
+          this.material_storage[input as keyof typeof MATERIALS].update(
+            (material) =>
+              Math.max(material - Math.floor(value * successRate), 0)
+          );
+        });
+
+        Object.entries(building.output).forEach(([output, value]) => {
+          this.material_storage[output as keyof typeof MATERIALS].update(
+            (material) => material + Math.floor(value * successRate)
+          );
         });
       });
+
+      this.currentMaterialIndex =
+        (this.currentMaterialIndex + 1) % MATERIALS_GENERATION_ORDER.length;
     }
   }
 
