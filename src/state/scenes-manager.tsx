3a17af9b5f289b4ddf8530a6ad6e9f2d3eef4bcd
Victor Debone <victor@debone.com.br>
2025-05-10 23:54:52 +0200

    feat: scenes manager
    a centralized place to manage scene transitions and effects

---
+++ src/state/scenes-manager.tsx
@@ -0,0 +1,93 @@
+import { signal } from "@game/core/signals/signals";
+import { SCENES } from "@game/scenes/scenes";
+import { MotionMachine } from "../core/motion-machine/motion-machine";
+
+export type SceneStates = "loading" | "intro" | "game" | "end";
+export type SceneEvents = "loaded" | "start" | "end";
+
+export class ScenesManager extends Phaser.Plugins.BasePlugin {
+  scenePlugin: Phaser.Scenes.ScenePlugin;
+  scenes: MotionMachine<SceneStates, SceneEvents>;
+
+  transitionSignal = signal(0);
+
+  constructor(pluginManager: Phaser.Plugins.PluginManager) {
+    super(pluginManager);
+  }
+
+  boot(sceneManager: Phaser.Scenes.ScenePlugin) {
+    this.scenePlugin = sceneManager;
+
+    this.scenes = (
+      <motionMachine initialState="preloader">
+        <state id="preloader">
+          <animation on="enter">
+            <step
+              run={() => {
+                this.scenePlugin.launch(SCENES.PRELOADER);
+              }}
+            />
+          </animation>
+          <transition on="loaded" target="intro" />
+        </state>
+        <state id="intro">
+          <animation on="enter">
+            <step
+              run={() => {
+                this.scenePlugin.launch(SCENES.INTRO);
+              }}
+            />
+          </animation>
+          <transition on="start" target="game" />
+        </state>
+        <state id="game">
+          <animation on="enter">
+            <step
+              run={() => {
+                this.scenePlugin.launch(SCENES.TRANSITIONS);
+                this.scenePlugin.bringToTop(SCENES.TRANSITIONS);
+              }}
+            />
+            <tween
+              signal={this.transitionSignal}
+              from={0}
+              to={1}
+              duration={1000}
+            />
+            <step
+              run={() => {
+                this.scenePlugin.stop(SCENES.TRANSITIONS);
+              }}
+            />
+          </animation>
+          <animation on="active">
+            <step
+              run={() => {
+                this.scenePlugin.launch(SCENES.GAME);
+              }}
+            />
+          </animation>
+          <transition on="end" target="end" />
+        </state>
+        <state id="end">
+          <animation on="exit">
+            <step
+              run={() => {
+                this.scenePlugin.stop(SCENES.THREE_COMET);
+              }}
+            />
+          </animation>
+        </state>
+      </motionMachine>
+    );
+  }
+
+  transitionTo(scene: SceneEvents) {
+    console.log("transitioning to", scene);
+    this.scenes.transition(scene);
+  }
+
+  getTransitionSignal() {
+    return this.transitionSignal;
+  }
+}
